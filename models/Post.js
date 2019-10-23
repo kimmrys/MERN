f1a3de4ccbe9d19a6afde97400b460a01463738d
Kimberly Reyes <kimberly@ubx.ph>
2019-10-23 13:57:21 +0800

    Add Post

--- models/Post.js
+++ models/Post.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const PostSchema = newSchema({
+const PostSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
     ref: 'users'
